177f924ab0d83613d5876378123a80f80f2e0b94
ruizajdev <juan.ruiz@pretzel.io>
2022-07-01 00:45:25 -0700

    search functionality

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import { useEffect, useState } from "react";
 import { fetchAllPokemon } from "./api";
 
 function App() {
-    const [pokemonIndex, setPokemonIndex] = useState([])
     const [pokemon, setPokemon] = useState([])
     const [searchValue, setSearchValue] = useState('')
     const [pokemonDetails, setPokemonDetails] = useState()
@@ -12,21 +11,19 @@ function App() {
             const {results: pokemonList} = await fetchAllPokemon()
 
             setPokemon(pokemonList)
-            setPokemonIndex(pokemonList)
         }
 
         fetchPokemon().then(() => {
             /** noop **/
         })
-    }, [searchValue])
+    }, [])
+
+    const pokemonIndex = searchValue.length === 0 ?
+        pokemon : pokemon.filter(monster => monster.name.toLowerCase().startsWith(searchValue.toLowerCase()))
 
     const onSearchValueChange = (event) => {
         const value = event.target.value
         setSearchValue(value)
-
-        setPokemon(
-            pokemonIndex.filter(monster => !monster.name.includes(value))
-        )
     }
 
     const onGetDetails = (name) => async () => {
@@ -39,22 +36,27 @@ function App() {
                 <input value={searchValue} onChange={onSearchValueChange} placeholder={'Search Pokemon'}/>
             </div>
             <div className={'pokedex__content'}>
-                {pokemon.length > 0 && (
-                    <div className={'pokedex__search-results'}>
-                        {
-                            pokemon.map(monster => {
-                                return (
-                                    <div className={'pokedex__list-item'} key={monster.name}>
-                                        <div>
-                                            {monster.name}
+                {pokemonIndex.length > 0 ?
+                    (
+                        <div className={'pokedex__search-results'}>
+                            {
+                                pokemonIndex.map(monster => {
+                                    return (
+                                        <div className={'pokedex__list-item'} key={monster.name}>
+                                            <div>
+                                                {monster.name}
+                                            </div>
+                                            <button onClick={onGetDetails(monster.name)}>Get Details</button>
                                         </div>
-                                        <button onClick={onGetDetails(monster.name)}>Get Details</button>
-                                    </div>
-                                )
-                            })
-                        }
-                    </div>
-                )}
+                                    )
+                                })
+                            }
+                        </div>
+                    ) :
+                    (
+                        <p>No Results Found</p>
+                    )
+                }
                 {
                     pokemonDetails && (
                         <div className={'pokedex__details'}>
